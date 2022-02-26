83654b483513ae71b4c0d4ca9674b2d0bb9e8ec5
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2022-02-26 22:08:50 +0100

    Add a test that generates time/loss data files of backprop and horde-ad

--- test/PlotTest.hs
+++ test/PlotTest.hs
@@ -2,10 +2,13 @@ module Main (main) where
 
 import Prelude
 
+import Control.DeepSeq
+import System.Random
 import Test.Tasty
+import Test.Tasty.HUnit hiding (assert)
 
-import qualified BenchMnistTools
 import           HordeAd.Tool.MnistTools
+import qualified MnistBackpropTools
 import qualified TestMnistFC
 
 main :: IO ()
@@ -13,12 +16,13 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Plot Tests"
-  [ testCase "hmatrix/manual 500 150 to file" do
-      trainData <- loadMnistData trainGlyphsPath trainLabelsPath
-      let testData = shuffle (mkStdGen 6) testData0
-      plot500150 testData
-  , mnistTestCase2T True
-                    "1 epoch, all batches, TL, wider, to file"
-                    1 60000 nnMnistLoss2L 500 150 0.02
-                    3.73e-2
+  [ testCase "hmatrix/manual 500 150 to file" $ do
+      trainData0 <- loadMnistData trainGlyphsPath trainLabelsPath
+      let !trainData = force $ shuffle (mkStdGen 6) trainData0
+      MnistBackpropTools.plot500150 trainData
+  , TestMnistFC.mnistTestCase2T
+      True
+      "2 epochs, all batches, TL, wider, to file"
+      2 60000 nnMnistLoss2L 500 150 0.02
+      4.290000000000005e-2
   ]
